f1efe9692c3ea24ccd3fdf8e7e4b3a331270d773
박유경 <pyk710_@naver.com>
2025-05-22 18:00:15 +0900

    [Bronze V] Title: 윤년, Time: 96 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,13 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+
+const a = Number(input[0]);
+
+if (a % 4 == 0) {
+    if (a % 100 != 0 || a % 400 == 0) {
+        console.log("1");
+    } else {
+        console.log("0");
+    }
+} else {
+    console.log("0");
+}
