0efec69714a52ef7fad467c944ded1f49bffdb62
박유경 <pyk710_@naver.com>
2025-05-22 18:06:54 +0900

    [Bronze V] Title: 시험 성적, Time: 144 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -0,0 +1,15 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+
+const score = Number(input);
+
+if(90 <= score && score <= 100){
+    console.log("A");
+} else if(80 <= score && score <= 89){
+    console.log("B");
+} else if(70 <= score && score <= 79){
+    console.log("C");
+} else if(60 <= score && score <= 69){
+    console.log("D");
+} else {
+    console.log("F");
+}
