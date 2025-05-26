4cae01431b5e671f31f3bf919c0a207e346a3eb9
박유경 <pyk710_@naver.com>
2025-05-26 17:43:24 +0900

    [Bronze V] Title: A+B - 5, Time: 108 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+for (let i = 0; i < input.length; i++) {
+    const [A, B] = input[i].split(' ').map(Number);
+    if (A === 0 && B === 0) break;
+    console.log(A + B);
+}
