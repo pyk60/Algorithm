39c56adbfa126746b6064ae7d2532ff2a93f92e5
박유경 <pyk710_@naver.com>
2025-05-28 20:37:36 +0900

    [Bronze IV] Title: 숫자의 합, Time: 152 ms, Memory: 9580 KB -BaekjoonHub

---
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.js
@@ -0,0 +1,11 @@
+const input = require('fs').readFileSync('dev/stdin').toString().trim().split('\n');
+
+const N = input[0];
+const numbers = input[1];
+
+let sum = 0;
+for (let i = 0; i < N; i++) {
+    sum += Number(numbers[i]);
+}
+
+console.log(sum);
